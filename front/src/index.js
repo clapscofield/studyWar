7ac4a5a4ecde3d33a95eabefe7f984eb36f21979
clapscofield <clarisse.scofield@gmail.com>
2021-01-10 14:19:49 -0300

    #002 Criação da base da pagina de cadastro e adicao da rota no botão

--- front/src/index.js
+++ front/src/index.js
@@ -28,11 +28,13 @@ import LandingPage from "views/examples/LandingPage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 import PaginaInicial from "features/paginaInicial";
+import CadastroInstituicao from "features/cadastroInstituicao";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/pagina-inicial" render={(props) => <PaginaInicial {...props} />} />
+      <Route path="/cadastro-instituicao" render={(props) => <CadastroInstituicao {...props} />} />
       <Route path="/components" render={(props) => <Index {...props} />} />
       <Route
         path="/landing-page"
