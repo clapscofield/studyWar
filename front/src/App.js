5a8bcf65a8595986837c019044222930a3062423
clapscofield <clarisse.scofield@gmail.com>
2021-01-10 21:29:40 -0300

    #002 Correção do erro de Router

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
