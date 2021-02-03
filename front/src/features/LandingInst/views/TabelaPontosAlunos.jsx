e0f19d3eb47caa380414956e1a1d6af9242996f0
Polanczyk <48301242+Polanczyk@users.noreply.github.com>
2021-02-03 12:06:37 -0300

    display da tabela

--- front/src/features/LandingInst/views/TabelaPontosAlunos.jsx
+++ front/src/features/LandingInst/views/TabelaPontosAlunos.jsx
@@ -9,6 +9,20 @@ const TabelaPontosAlunos = (props) => {
   const [alunos, setAlunos] = useState();
 
   useEffect(() => {
+            setAlunos( [
+                { _id: 1, nome: 'Cedrico', matricula: 1, minutosEstudados: 180, idEquipe: "Lufa-Lufa" },
+                { _id: 2, nome: 'Luna', matricula: 2, minutosEstudados: 90, idEquipe : "Corvinal" },
+                { _id: 3, nome: 'Draco', matricula: 3, minutosEstudados: 90, idEquipe : "Sonserina" },
+                { _id: 4, nome: 'Harry', matricula: 4, minutosEstudados: 120, idEquipe : "Grifinória" },
+                { _id: 5, nome: 'Hermione', matricula: 5, minutosEstudados: 600, idEquipe : "Grifinória" },
+                { _id: 6, nome: 'Goyle', matricula: 6, minutosEstudados: 15, idEquipe : "Sonserina" },
+          ])
+  }, []);
+
+
+
+  /*
+  useEffect(() => {
     const obterDadosAlunos = async (idInstituicaoBusca) => {
       await LandingInstManager.ObterAlunosPorIdInstituicao(
         idInstituicaoBusca
@@ -19,7 +33,7 @@ const TabelaPontosAlunos = (props) => {
       });
     };
     obterDadosAlunos("clap.inst");
-  }, []);
+  }, []);*/
 
   // useEffect(() => {
   //   const obterDadosAlunos = async () => {
@@ -49,7 +63,7 @@ const TabelaPontosAlunos = (props) => {
                 <td>{aluno.matricula}</td>
                 <td>{aluno.nome}</td>
                 <td>{aluno.idEquipe}</td>
-                <td className="text-center">{aluno.minutosEstudados}</td>
+                <td className="text-center">{aluno.minutosEstudados / 60}</td>
                 <td className="text-center">{aluno.minutosEstudados * 23}</td>
                 <td />
               </tr>
